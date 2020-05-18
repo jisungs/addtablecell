68331ee3245baf21a5d56b1f8d1d1dc51987d522
jisung <jisung.smart@gmail.com>
2020-05-18 14:48:01 +0900

    initial commit

---
+++ TableViewTest/ThirtyDayCell.swift
@@ -0,0 +1,141 @@
+//
+//  ThirtyDayCell.swift
+//  TableViewTest
+//
+//  Created by The book Air on 2020/05/14.
+//  Copyright © 2020 The book Air. All rights reserved.
+//
+
+import UIKit
+
+class ThirtyDayCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:)has been implemented")
+    }
+    
+    func setupView() {
+        addSubview(cellView)
+        cellView.addSubview(dayLabel)
+        cellView.addSubview(textField)
+        cellView.addSubview(addButton)
+        self.selectionStyle = .none
+        
+        NSLayoutConstraint.activate([
+            cellView.topAnchor.constraint(equalTo: self.topAnchor, constant: 20),
+            cellView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -10),
+            cellView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10),
+            cellView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+
+        ])
+        
+        dayLabel.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        dayLabel.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        dayLabel.centerYAnchor.constraint(equalTo: cellView.centerYAnchor).isActive = true
+        dayLabel.leftAnchor.constraint(equalTo: cellView.leftAnchor, constant: 20).isActive = true
+        
+        textField.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        textField.widthAnchor.constraint(equalToConstant: 250).isActive = true
+        textField.centerYAnchor.constraint(equalTo: cellView.centerYAnchor).isActive = true
+        textField.leftAnchor.constraint(equalTo: cellView.leftAnchor, constant: 20).isActive = true
+        
+        addButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        addButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        addButton.centerYAnchor.constraint(equalTo: cellView.centerYAnchor).isActive = true
+        addButton.leftAnchor.constraint(equalTo: cellView.leftAnchor, constant: 280).isActive = true
+    }
+    
+    
+    
+    let  cellView : UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.cyan
+        view.layer.cornerRadius = 10
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let dayLabel : UILabel  = {
+        let label = UILabel()
+        label.text = "Day 1"
+        label.textColor = UIColor.white
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let addButton : UIButton = {
+        let button = UIButton(frame: CGRect(x: 200, y: 25, width: 100, height: 30))
+        button.backgroundColor = UIColor.blue
+        button.layer.cornerRadius = 10
+        button.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        button.layer.shadowOffset = CGSize(width: 0.0, height: 3.0)
+        button.layer.shadowOpacity = 1.0
+        button.layer.masksToBounds = false
+        button.layer.shadowRadius = 10
+        button.layer.cornerRadius = 4.0
+        button.setTitle("Add Button", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    let textField : UITextField = {
+        let textField = UITextField(frame: CGRect(x: 10, y: 25, width: 200, height: 30))
+        textField.backgroundColor = UIColor.white
+        textField.layer.cornerRadius = 10
+        textField.layer.borderWidth = 0.6
+        textField.isUserInteractionEnabled = true
+        textField.textAlignment = .center
+        textField.placeholder = "guest name"
+        textField.font = UIFont.systemFont(ofSize: 15)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+
+    
+}
+
+extension UIView {
+    func blink(duration: TimeInterval = 0.5, delay: TimeInterval = 0.0, alpha: CGFloat = 0.0) {
+        UIView.animate(withDuration: duration, delay: delay, options: [.curveEaseInOut, .repeat, .autoreverse], animations: {
+            self.alpha = alpha
+        })
+    }
+}
+
+extension UIButton {
+    open override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        return self.bounds.contains(point) ? self : nil
+    }
+    func blink(enabled: Bool = true, duration: CFTimeInterval = 1.0, stopAfter: CFTimeInterval = 0.0 ) {
+        enabled ? (UIView.animate(withDuration: duration, //Time duration you want,
+            delay: 0.0,
+            options: [.curveEaseInOut, .autoreverse, .repeat],
+            animations: { [weak self] in self?.alpha = 0.0 },
+            completion: { [weak self] _ in self?.alpha = 1.0 })) : self.layer.removeAllAnimations()
+        if !stopAfter.isEqual(to: 0.0) && enabled {
+            DispatchQueue.main.asyncAfter(deadline: .now() + stopAfter) { [weak self] in
+                self?.layer.removeAllAnimations()
+            }
+        }
+    }
+}
+
--- TableViewTest/ViewController.swift
+++ TableViewTest/ViewController.swift
@@ -8,13 +8,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    var guestNameArray = [String]()
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupTableView()
+        
+    }
+    
+    func setupTableView(){
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(ThirtyDayCell.self, forCellReuseIdentifier: "cellId")
+        
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            tableView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+            tableView.leftAnchor.constraint(equalTo: self.view.leftAnchor)
+            
+        ])
+    }
+    
+    let tableView: UITableView = {
+        let tv = UITableView()
+        tv.backgroundColor = UIColor.white
+        tv.separatorColor = UIColor.white
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        return tv
+    }()
+ 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return 10
+       }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) as! ThirtyDayCell
+        cell.backgroundColor = UIColor.white
+        //cell.textField.placeholder = "guest Name"
+        cell.dayLabel.text = "Day\(indexPath.row + 1)"
+        cell.addButton.addTarget(self, action: #selector(buttonAction(_:)), for: .touchUpInside)
+        return cell
+       }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
 
 
+    
+    @objc func buttonAction(_ sender: UIButton!){
+        let buttonPosition = sender.convert(CGPoint.zero , to: tableView)
+        let indexPath = tableView.indexPathForRow(at: buttonPosition)
+        let cell = tableView.cellForRow(at: indexPath!) as! ThirtyDayCell
+      
+        if let guestName = cell.textField.text {
+            guestNameArray.append(guestName)
+        }
+    }
+    
 }
 
